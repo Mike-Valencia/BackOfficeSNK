d585221b854d7b2c84fd85af75fe51864931f4f0
Mike-Valencia <espanyol.mike@gmail.com>
2017-07-30 19:47:55 -0300

    Agregando backoffice

---
+++ CentralBack/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>BackOffice</title>
+	<!-- Includes -->
+	<?php include "includes/_inc.php";?>
+</head>
+<body>
+	<div id="header">
+		En este div va el header
+	</div>
+	<div>
+		En este el lateral izquierdo
+	</div>
+	<div>
+		En este va el contendio de cada menu del lateral
+	</div>
+</body>
+</html>
